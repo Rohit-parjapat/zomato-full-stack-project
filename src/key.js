388fd250fcfa583314a3b268ae092e0d75a374e8
Rohit-parjapat <rparjapati7377@gmail.com>
2023-08-09 23:57:57 +0530

    Url changed

--- src/key.js
+++ src/key.js
@@ -1,6 +1,6 @@
 export const API_URL = window.location.hostname.includes("localhost")
   ? "http://localhost:4000"
-  : "https://poised-yak-hose.cyclic.app/";
+  : "https://poised-yak-hose.cyclic.app";
 
 export const CLIENT_URL = window.location.hostname.includes("localhost")
   ? "http://localhost:3000"
