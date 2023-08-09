c5da8b10470afbd993b2841fe9ab48e5b7758cb8
Rohit-parjapat <rparjapati7377@gmail.com>
2023-08-09 23:49:35 +0530

    Url changed

--- src/key.js
+++ src/key.js
@@ -1,9 +1,13 @@
-export const API_URL = window.location.hostname.includes("localhost")
+export const API_URL = window.location.hostname.includes(
+  "https://zomato-cl.netlify.app/"
+)
   ? "https://poised-yak-hose.cyclic.app"
   : "/node";
 
-export const CLIENT_URL = window.location.hostname.includes("localhost")
-  ? "http://localhost:3000"
+export const CLIENT_URL = window.location.hostname.includes(
+  "https://zomato-cl.netlify.app/"
+)
+  ? "hhttps://zomato-cl.netlify.app/"
   : "/";
 
 // module.exports = API_URL;
