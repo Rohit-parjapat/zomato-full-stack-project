c18d66355a1f1b45cfa4b74a484b85dd08978630
Rohit-parjapat <rparjapati7377@gmail.com>
2023-08-09 23:58:29 +0530

    url changed

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ const zomato = express();
 privateRouteConfig(passport);
 googleAuthConfig(passport);
 
-zomato.use(cors({ origin: "http://localhost:3000" }));
+zomato.use(cors({ origin: "https://zomato-cl.netlify.app/" }));
 zomato.use(helmet());
 zomato.use(express.json());
 zomato.use(
