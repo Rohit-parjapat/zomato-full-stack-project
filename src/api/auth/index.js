d6ad5e98de25e0910f39cd140b1f3a6e384402a4
Rohit-parjapat <rparjapati7377@gmail.com>
2023-08-10 15:20:53 +0530

    auth api url changed

--- src/api/auth/index.js
+++ src/api/auth/index.js
@@ -69,11 +69,11 @@ Router.get(
   passport.authenticate("google", { failureRedirect: "/" }),
   (req, res) => {
     // return res.status(200).json({
-    //     token: req.session.passport.user.token,
+    //   token: req.session.passport.user.token,
     // });
 
     return res.redirect(
-      `http://localhost:3000/google/${req.session.passport.user.token}`
+      `https://zomato-cl.netlify.app/google/${req.session.passport.user.token}`
     );
   }
 );
